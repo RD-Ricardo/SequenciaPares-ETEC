fd3ea89a696e2d1000f23aa6282009b77adda4b5
RD-Ricardo <ricardodias169@gmail.com>
2021-05-27 15:17:28 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("--Sequencia de Pares--");
+            Console.WriteLine("---------------------\n");
+            Console.Write("Números pares entre 0 e...: ");
+            int num =  Convert.ToInt32(Console.ReadLine());
+            for(int i  = 0; i<num;i++)
+            {
+                if(i % 2 == 0)
+                {
+                   Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
